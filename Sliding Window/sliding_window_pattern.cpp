568897b3eb19e189ee19f1d31755058a12bf955c
Ruthvik Bhaskar <ruthvik.bhaskar2006@gmail.com>
2025-10-12 21:52:57 +0530

    Create sliding_window_pattern.cpp

---
+++ Sliding Window/sliding_window_pattern.cpp
@@ -0,0 +1,13 @@
+int l = 0;
+for (int r = 0; r < n; r++) {
+    // expand window
+    add element to window
+
+    while (window invalid) {
+        remove element from left
+        l++;
+    }
+
+    update maxlen/minlen
+}
+
